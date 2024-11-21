3d6bd6f6a2231cdbfefc06c136a1795c5f96a449
Dat Nguyen <datnguyenn626@gmail.com>
2024-11-20 19:13:52 -0600

    updated files

--- masterServer.java
+++ masterServer.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -12,7 +14,7 @@ public class masterServer {
         try {
             ServerSocket socket = new ServerSocket(8000);
             boolean listening = true;
-        
+
             while (listening){
                 new Thread (new Handler (socket.accept())).start();
             }
@@ -42,24 +44,36 @@ class Handler implements Runnable {
             BufferedReader br = new BufferedReader (new InputStreamReader (client.getInputStream()));
             PrintWriter pw = new PrintWriter (client.getOutputStream(), true);
 
-            // while ((line = br.readLine()) != null){
-            //     System.out.println("Client says: " + line); 
-            //     pw.println("Server received: " + line);   
-            // }
+            DataInputStream input = new DataInputStream(client.getInputStream());
+            DataOutputStream output = new DataOutputStream(client.getOutputStream());
 
             while (!client.isClosed()) {
-                if (br.ready()) {
-                    String incoming = br.readLine();
-                    if (incoming != null) {
-                        System.out.println("Client says: " + line);
-                        pw.println("Server received: " + line);
+                if (input.available() > 0){
+                    int size = input.readInt();
+                    int [] values = new int [size];
+
+                    for (int i = 0; i < size; i++){
+                        values[i] = input.readInt();
                     }
+
+                    System.out.println("Received numbers: " + java.util.Arrays.toString(values));
+                    int sum = 0;
+                    for (int num : values) {
+                        sum += num;
                 }
+
+                output.writeUTF("Server received " + size + " numbers. Sum: " + sum);
+                }
+
+                
 
                 pw.println("ping");
                 System.out.println("Sent: ping");
+                
 
                 TimeUnit.SECONDS.sleep(5);
+                input.close();
+                output.close();
             }
         }
         catch (Exception ex){
@@ -68,6 +82,7 @@ class Handler implements Runnable {
         finally {
             try {
                 client.close();
+            
             } catch (IOException ex) {
                 System.out.println("Error closing client socket: " + ex.getMessage());
             }
